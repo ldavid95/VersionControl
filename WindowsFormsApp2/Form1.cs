5ce28cbf43c09f6a049e4c7d156a5822aded13f8
David Lazar <lazar.david95@gmail.com>
2021-10-22 20:08:49 +0200

    5

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp2.Entities;
 
 namespace WindowsFormsApp2
 {
@@ -14,12 +15,29 @@ namespace WindowsFormsApp2
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
 
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+            /*
+             * PortfolioItem p = new PortfolioItem();
+             * p.Index = "OTP";
+             * p.Volume = 10;
+             * Portfolio.Add(p);
+             */
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
