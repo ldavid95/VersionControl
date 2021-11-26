35d118afef2d7fc95cb6c55a0608572d42f21b6a
David Lazar <lazar.david95@gmail.com>
2021-11-26 18:10:55 +0100

    g

---
+++ UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class AccountControllerTestFixture
+{
+        [
+                    Test,
+                    TestCase("abcd1234", false),
+                    TestCase("irf@uni-corvinus", false),
+                    TestCase("irf.uni-corvinus.hu", false),
+                    TestCase("irf@uni-corvinus.hu", true)
+        ]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
