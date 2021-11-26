ec914fa3a6e532c94b2be4d477dc1a489a2df56b
David Lazar <lazar.david95@gmail.com>
2021-11-26 17:51:07 +0100

    q

--- UnitTestExample/Properties/AssemblyInfo.cs
+++ UnitTestExample/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsFormsApp2")]
+[assembly: AssemblyTitle("UnitTestExample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsFormsApp2")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyProduct("UnitTestExample")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f35b840b-ba72-493c-9725-d67f2fbfc521")]
+[assembly: Guid("f1fbdbb6-feba-421b-a647-484ed380e500")]
 
 // Version information for an assembly consists of the following four values:
 //
